0b177097b29f86a4a0b58d9970c0fc35d6630ea0
Slava <fomenkoslav@gmail.com>
2018-02-08 17:18:41 +0200

    blog start

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+#from django.http import HttpResponse
+# import datetime
+
+
+def fib(num):
+    fib_list = [0, 1]
+    for i in range(num - 2):
+        fib_list.append(fib_list[i] + fib_list[i + 1])
+
+    return fib_list
+
+
+def post_list(request):
+    #html = ', '.join(str(x) for x in fib(8))
+    #html = 1/3
+    #return HttpResponse(html)
+    return render(request, 'blog/post_list.html', {})
+
+    #   now = datetime.datetime.now()
+    #   html = "<html><body>It is now %s.</body></html>" % now
+    #   return HttpResponse(html)
